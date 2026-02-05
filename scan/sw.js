e6b893fcd8c16a886d9f2d0257775ccc71ad3ce5
Qoimun <herabagus9@gmail.com>
2026-02-05 10:30:11 +0700

    Update sw.js

--- scan/sw.js
+++ scan/sw.js
@@ -1,10 +1,18 @@
 const CACHE_NAME = "scan-offline-v1";
-const FILES = ["./scan.html", "./scan.js"];
+const FILES = [
+  "./scan.html",
+  "./scan.js",
+  "./manifest.json",
+  "https://unpkg.com/html5-qrcode",
+  "https://actions.google.com/sounds/v1/cartoon/wood_plank_flicks.ogg",
+  "https://actions.google.com/sounds/v1/alarms/beep_short.ogg",
+  "https://actions.google.com/sounds/v1/alarms/alarm_clock.ogg"
+];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES)));
+self.addEventListener("install", (e)=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(FILES)));
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+self.addEventListener("fetch", (e)=>{
+  e.respondWith(caches.match(e.request).then(res=>res || fetch(e.request)));
 });
